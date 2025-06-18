9a337a825340af5b147e848edb25a6a12c2c4f2b
ChInPen <azse870225@gmail.com>
2025-06-19 03:40:47 +0800

    NO.26

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'build')));
 app.use('/images', express.static(path.join(__dirname, 'build/images')));
 // 抓所有前端路徑
-app.get('/*any', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
