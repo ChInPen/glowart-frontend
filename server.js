9a0f937fd8a6d4c3370d81794e4dd3441488a87c
ChInPen <azse870225@gmail.com>
2025-06-19 03:48:10 +0800

    NO.27

--- server.js
+++ server.js
@@ -2,12 +2,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
-
 app.use(express.static(path.join(__dirname, 'build')));
-app.use('/images', express.static(path.join(__dirname, 'build/images')));
-// 抓所有前端路徑
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
