59111d504b2556779a6817e3ed231ee2e8ef467b
ChInPen <azse870225@gmail.com>
2025-06-19 04:08:12 +0800

    NO.29

--- build.js
+++
@@ -1,9 +0,0 @@
-// build.js
-const fs = require("fs");
-const path = require("path");
-
-const src = path.join(__dirname, "public", "_redirects");
-const dest = path.join(__dirname, "build", "_redirects");
-
-fs.copyFileSync(src, dest);
-console.log("✅ _redirects copied to build folder");
